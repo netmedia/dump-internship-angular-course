bf5496a6f370829f753cf456fc73c9cc9c24a6db
dbettini <dbettini91@gmail.com>
2017-06-19 20:58:07 +0200

    Make user email unique

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { ObjectId } = mongoose.Schema.Types;
 
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   isAdmin: { type: Boolean, required: true, default: false },
   rentedItems: [{ type: ObjectId, required: true, ref: "Item" }]
