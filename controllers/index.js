614113b1c6344caf9f654afb4a80f76dc47397a9
dbettini <dbettini91@gmail.com>
2017-06-19 14:19:16 +0200

    Refactor initControllers to be more generic
    - now getDirectoryModulePaths
    - controllers now initialized in controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 const routerFactory = require("express").Router;
-const initControllers = require("../common/initControllers");
+const getDirectoryModulePaths = require("../common/getDirectoryModulePaths");
 const { INTERNAL_SERVER_ERROR } = require("http-status");
 
 module.exports = app => {
   const apiRouter = routerFactory();
 
   app.use("/api", apiRouter);
-  initControllers(__dirname, apiRouter);
+  const modulePaths = getDirectoryModulePaths(__dirname);
+  modulePaths.forEach(m => require(m)(apiRouter, routerFactory()));
 
   app.use((err, req, res, next) => {
     console.error(err.stack);
